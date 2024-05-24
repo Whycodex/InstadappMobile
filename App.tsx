3d5d8f4b2dd2c8509444323939be9af1b7939fe7
Whycodex <neerajgupta12300@gmail.com>
2024-05-25 01:50:51 +0530

    Added webview screen

--- App.tsx
+++ App.tsx
@@ -1,18 +1,42 @@
-import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useRef, useState } from "react";
+import { StatusBar, BackHandler } from "react-native";
 import { SafeAreaView, StyleSheet } from "react-native";
 import WebView from "react-native-webview";
 
 export default function App() {
+  const webViewRef = useRef(null);
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  const handleBackButton = () => {
+    if (canGoBack && webViewRef.current) {
+      (webViewRef.current as any).goBack();
+      return true;
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener("hardwareBackPress", handleBackButton);
+    return () => {
+      BackHandler.removeEventListener("hardwareBackPress", handleBackButton);
+    };
+  }, [canGoBack]);
+
   return (
     <SafeAreaView style={styles.container}>
-      <WebView source={{ uri: "https://instadapp.io/" }} style={{ flex: 1 }} />
-      <StatusBar style="light" />
+      <WebView
+        ref={webViewRef}
+        source={{ uri: "https://instadapp.io/" }}
+        style={{ flex: 1 }}
+        onNavigationStateChange={(navState) => setCanGoBack(navState.canGoBack)}
+      />
+      <StatusBar />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
 });
